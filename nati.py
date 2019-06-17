3eb03e97ed650519f5ca714711d8049e2d073605
Sean <hsj106@naver.com>
2019-06-17 22:24:15 +0900

    feat: Add topstarnews & Fix tistory download

--- nati.py
+++ nati.py
@@ -97,6 +97,88 @@ class Worker(QObject):
 
         self.finished_err.emit(['4', '0', na_title, '', url])
         QThread.msleep(1300)
+
+    @pyqtSlot()
+    def topstarnews(self, url, sprt, path, status):
+        try:
+            res = req.get(url, headers=self._header)
+            topstarSoup = BeautifulSoup(res.text, "html.parser")
+        except:
+            self.finished_err.emit(['4', '0', '', '로드 실패', url])
+            QThread.msleep(1300)
+            return
+
+        if topstarSoup.find("meta", {"property": "og:title"}) is None:
+            self.finished_err.emit(['4', '0', '', '로드 실패', url])
+            QThread.msleep(1300)
+            return
+        
+        top_title = '[Topstarnews] ' + topstarSoup.find("meta", {"property": "og:title"}).get("content").strip()
+        top_title = top_title = re.sub("[/\\:*?\"<>|.]", "_", top_title)
+        top_title = re.sub("\n", "_", top_title)
+
+        if not os.path.isdir(path):
+            try:
+                os.makedirs(path)
+            except Exception as E:
+                self.finished_err.emit(['2', E])
+                return
+
+        img_tag = topstarSoup.find("div", {"class": "user-content"}).find_all('img')
+
+        for i in range(0, len(img_tag)):
+            img_url = img_tag[i].get('src')
+            if img_url is None: # No image tag
+                continue
+
+            img_url = 'http://www.topstarnews.net' + img_url
+            img_name = urlparse.unquote(img_url.split('/')[-1])
+            if img_name[-4:] == '.jpg':
+                img_url = 'http://www.topstarnews.net' + img_tag[i].get('data-org')
+                img_name = urlparse.unquote(img_url.split('/')[-1])
+            elif not img_name[-4:] == '.gif':
+                continue
+
+            self.finished.emit('다운로드 중 (%s): %s' % (status, img_name))
+
+            if sprt:
+                if not os.path.isdir('%s%s' % (path, top_title)):
+                    try:
+                        os.makedirs('%s%s' % (path, top_title))
+                    except Exception as E:
+                        self.finished_err.emit(['2', E])
+                        return
+
+                try:
+                    with open('%s%s\\%s' % (path, top_title, img_name), "wb") as file:
+                        img = req.get(img_url, headers=self._header)
+                        file.write(img.content)
+                        file.close()
+                    self.finished_err.emit(['4', '1', img_name, '성공', img_url])
+                except Exception as E:
+                    file.close()
+                    print('download image \n %s' % str(E))
+                    self.finished_err.emit(['4', '1', img_name, '실패', img_url])
+                    return
+            else:
+                try:
+                    with open('%s%s' % (path, img_name), "wb") as file:
+                        img = req.get(img_url, headers=self._header)
+                        file.write(img.content)
+                        file.close()
+                    self.finished_err.emit(['4', '1', img_name, '성공', img_url])
+                except Exception as E:
+                    file.close()
+                    print('download image \n %s' % str(E))
+                    self.finished_err.emit(['4', '1', img_name, '실패', img_url])
+                    return
+
+            QThread.msleep(500)
+
+        self.finished_err.emit(['4', '0', top_title, '', url])
+        QThread.msleep(1300)
+
+
         
     @pyqtSlot()
     def tistory(self, url, sprt, path, status):
@@ -113,7 +195,7 @@ class Worker(QObject):
             QThread.msleep(1300)
             return
 
-        ti_title = '[Tistory] ' + tistorySoup.find("title").text.strip()
+        ti_title = '[Tistory] ' + tistorySoup.find("meta", {"property": "og:title"}).get("content").strip()
         ti_title = re.sub("[/\\:*?\"<>|.]", "_", ti_title) # Remove special characters from folder name
         ti_title = re.sub("\n", "_", ti_title) # Remove line feed character from folder name
 
@@ -124,18 +206,17 @@ class Worker(QObject):
                 self.finished_err.emit(['2', E])
                 return
 
-        # I have referred to the code at following address: https://github.com/tucuprum/jjal_downloader
-        # Thanks to tucuprum
         img_tag = tistorySoup.find_all('img')
         
         for i in range(0, len(img_tag)):
             img_url = img_tag[i].get('src')
-            img_name = img_tag[i].get('filename')
+            img_name = img_tag[i].get('data-filename') # New domain (k.kakaocdn.net)
+            if img_name is None: # Maybe old domain (t1.daumcdn.net)
+                img_name = img_tag[i].get('filename')
+                img_url += '?original'
 
-            if img_name is None: # This image isn't what you want to download.
+            if img_name is None: # This image isn't what you want to download or the page has no image.
                 continue
-
-            img_url += '?original'
             
             self.finished.emit('다운로드 중 (%s): %s' % (status, img_name))
             
@@ -187,6 +268,8 @@ class Worker(QObject):
         for i in range(0, len(self.search_link)):
             if 'post.naver.com' in self.search_link[i]: # Naver Post
                 self.naverPost(self.search_link[i], list_[1], list_[2], '%s/%s' % (i + 1, len(self.search_link)))
+            elif 'topstarnews.net' in self.search_link[i]: # Topstarnews
+                self.topstarnews(self.search_link[i], list_[1], list_[2], '%s/%s' % (i + 1, len(self.search_link)))
             else: # Tistory or Unknown URL
                 self.tistory(self.search_link[i], list_[1], list_[2], '%s/%s' % (i + 1, len(self.search_link)))
 
